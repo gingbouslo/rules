ad4764980363919cd5af2ec4865ca22622562524
evecus <jmfu003@gmail.com>
2025-11-24 22:02:52 +0800

    Update UFW rules to allow 192.168.0.0/16
    Consolidate allowed IP ranges for UFW rules.

--- sh/set-ufw-rules.sh
+++ sh/set-ufw-rules.sh
@@ -1,9 +1,7 @@
 apt install ufw
 ufw default deny incoming
 ufw default allow outgoing
-ufw allow from 192.168.1.0/24
-ufw allow from 192.168.0.0/24
-ufw allow from 192.168.31.0/24
+ufw allow from 192.168.0.0/16
 ufw allow from 172.16.0.0/12
 ufw allow 6666/tcp
 ufw enable
